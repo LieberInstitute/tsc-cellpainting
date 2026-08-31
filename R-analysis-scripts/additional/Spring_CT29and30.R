66179d8509e95945ea5e8432a531537e59348eb5
Frank Piscotta <161358089+fpiscotta@users.noreply.github.com>
2026-08-31 14:40:37 -0400

    Add files via upload

---
+++ R-analysis-scripts/additional/Spring_CT29and30.R
@@ -0,0 +1,96 @@
+setwd("C:\\Users\\frank.piscotta\\OneDrive - Lieber Institute for Brain Development\\Documents\\Cell Painting\\TSC Project\\TSC paper\\formal-Dx-reanalysis\\AUCs\\")
+
+#-----------------------------------------------------------------------------------------------------------
+#-----------------------------------Features----------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------
+
+#Normoxia
+features = read.csv("C:\\Users\\frank.piscotta\\OneDrive - Lieber Institute for Brain Development\\Documents\\Cell Painting\\TSC Project\\TSC paper\\Spring analysis\\combined embeddings\\combined embeddings - all oxygen - well - normalized by plate.csv")
+features[1:10] <- lapply(features[1:10],as.factor)
+
+features_normoxia <- features[features$Metadata_Oxygen == 'Normoxia',]
+
+models <- c("ranger","glmnet","mlp")
+
+folds <- list()
+folds[[1]] <- which(features_normoxia$Metadata_Cell_line != "19A" & features_normoxia$Metadata_Cell_line != "9B")
+folds[[2]] <- which(features_normoxia$Metadata_Cell_line != "9c1" & features_normoxia$Metadata_Cell_line != "8B")
+folds[[3]] <- which(features_normoxia$Metadata_Cell_line != "6A" & features_normoxia$Metadata_Cell_line != "11A")
+folds[[4]] <- which(features_normoxia$Metadata_Cell_line != "7c6" & features_normoxia$Metadata_Cell_line != "20A")
+folds[[5]] <- which(features_normoxia$Metadata_Cell_line != "1016.02" & features_normoxia$Metadata_Cell_line != "1A" & features_normoxia$Metadata_Cell_line != "13A")
+folds[[6]] <- which(features_normoxia$Metadata_Cell_line != "1002.01" & features_normoxia$Metadata_Cell_line != "3B" & features_normoxia$Metadata_Cell_line != "1006.02")
+folds[[7]] <- which(features_normoxia$Metadata_Cell_line != "7A" & features_normoxia$Metadata_Cell_line != "2A" & features_normoxia$Metadata_Cell_line != "12A")
+folds[[8]] <- which(features_normoxia$Metadata_Cell_line != "1009.04" & features_normoxia$Metadata_Cell_line != "4B")
+folds[[9]] <- which(features_normoxia$Metadata_Cell_line != "15B" & features_normoxia$Metadata_Cell_line != "16B")
+folds[[10]] <- which(features_normoxia$Metadata_Cell_line != "1013.04" & features_normoxia$Metadata_Cell_line != "17A")
+folds[[11]] <- which(features_normoxia$Metadata_Cell_line != "CT29" & features_normoxia$Metadata_Cell_line != "CT30")
+
+#Full dataset
+trainingSex <- features_normoxia[,-c(1:3,5:10)]
+
+train_control <- trainControl(method = "cv",
+                              index = folds,
+                              summaryFunction = twoClassSummary,
+                              classProbs = TRUE,
+                              savePredictions = TRUE)
+
+AUC3 <- data.frame(matrix(ncol=3,nrow=11))
+colnames(AUC3) <- c("ranger","glmnet","mlp")
+
+for (i in seq_along(models)) {
+  set.seed(120)
+  modelSex <- train(
+    Metadata_Sex ~ .,
+    data = trainingSex,
+    method = models[i],
+    trControl = train_control,
+    metric = "ROC",
+  )
+  
+  AUC3[,i] <- modelSex$resample$ROC
+}
+
+write.csv(AUC,file="caret_Sex_Normoxia_Spring.csv")
+
+#Hypoxia
+features_hypoxia <- features[features$Metadata_Oxygen == 'Hypoxia',]
+
+AUC4 <- data.frame(matrix(ncol=3,nrow=11))
+colnames(AUC4) <- c("ranger","glmnet","mlp")
+
+folds <- list()
+folds[[1]] <- which(features_hypoxia$Metadata_Cell_line != "19A" & features_hypoxia$Metadata_Cell_line != "9B")
+folds[[2]] <- which(features_hypoxia$Metadata_Cell_line != "9c1" & features_hypoxia$Metadata_Cell_line != "8B")
+folds[[3]] <- which(features_hypoxia$Metadata_Cell_line != "6A" & features_hypoxia$Metadata_Cell_line != "11A")
+folds[[4]] <- which(features_hypoxia$Metadata_Cell_line != "7c6" & features_hypoxia$Metadata_Cell_line != "20A")
+folds[[5]] <- which(features_hypoxia$Metadata_Cell_line != "1016.02" & features_hypoxia$Metadata_Cell_line != "1A" & features_hypoxia$Metadata_Cell_line != "13A")
+folds[[6]] <- which(features_hypoxia$Metadata_Cell_line != "1002.01" & features_hypoxia$Metadata_Cell_line != "3B" & features_hypoxia$Metadata_Cell_line != "1006.02")
+folds[[7]] <- which(features_hypoxia$Metadata_Cell_line != "7A" & features_hypoxia$Metadata_Cell_line != "2A" & features_hypoxia$Metadata_Cell_line != "12A")
+folds[[8]] <- which(features_hypoxia$Metadata_Cell_line != "1009.04" & features_hypoxia$Metadata_Cell_line != "4B")
+folds[[9]] <- which(features_hypoxia$Metadata_Cell_line != "15B" & features_hypoxia$Metadata_Cell_line != "16B")
+folds[[10]] <- which(features_hypoxia$Metadata_Cell_line != "1013.04" & features_hypoxia$Metadata_Cell_line != "17A")
+folds[[11]] <- which(features_hypoxia$Metadata_Cell_line != "CT29" & features_hypoxia$Metadata_Cell_line != "CT30")
+
+train_control <- trainControl(method = "cv",
+                              index = folds,
+                              summaryFunction = twoClassSummary,
+                              classProbs = TRUE,
+                              savePredictions = TRUE)
+
+#Full dataset
+trainingSex <- features_hypoxia[,-c(1:3,5:10)]
+
+for (i in seq_along(models)) {
+  set.seed(120)
+  modelSex <- train(
+    Metadata_Sex ~ .,
+    data = trainingSex,
+    method = models[i],
+    trControl = train_control,
+    metric = "ROC",
+  )
+  
+  AUC4[,i] <- modelSex$resample$ROC
+}
+
+write.csv(AUC,file="caret_Sex_Hypoxia_Spring.csv")
